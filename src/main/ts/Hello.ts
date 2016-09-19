5f36590e1a0424d6bd2099d5c5c90002ac2c1ce1
tei <takesoft@gmail.com>
2016-09-19 23:59:01 +0900

    compile success.

--- src/main/ts/Hello.ts
+++ src/main/ts/Hello.ts
@@ -1,4 +1,5 @@
-import {Greeter} from "Greeter";
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {Greeter} from "./Greeter";
 
 var greeter = new Greeter("world");
 alert(greeter);
